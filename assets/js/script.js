fe8404144a01093f54a508e6fa62b3553161ae64
Steven Green <stevengreen@Stevens-MacBook-Pro.local>
2026-04-28 20:33:21 -0700

    initial js files

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const playerCard = document.querySelector('#playerCard');
+const cpuCard = docuent.querySelector('#cpuCard');
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+const weapons = ["rock", "paper", "scissors"];
+
+
+
