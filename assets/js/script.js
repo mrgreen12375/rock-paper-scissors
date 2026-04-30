737a21c749a533fa6eada40a2d8ffe8402ebf007
Steven Green <stevengreen@Stevens-MacBook-Pro.local>
2026-04-29 18:25:27 -0700

    setup click events and results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 const playerCard = document.querySelector('#playerCard');
-const cpuCard = docuent.querySelector('#cpuCard');
+const cpuCard = document.querySelector('#cpuCard');
+
+const playerResults = document.querySelector('#playerResults');
+const cpuResults = document.querySelector('#cpuResults');
 
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
@@ -7,5 +10,48 @@ const scissors = document.querySelector('#scissors');
 
 const weapons = ["rock", "paper", "scissors"];
 
+let cpuWeapon;
+
+function playerChoice(playerWeapon){
+    playerCard.innerHTML = `<img class="player" src="./assets/images/${playerWeapon}.png"/>`;
+}
+
+function cpuChoice(){
+    const index = Math.floor(Math.random() * weapons.length);
+    cpuWeapon = weapons[index];
+
+    cpuCard.innerHTML = `<img class="cpu" src="./assets/images/${cpuWeapon}.png"/>`;
+}
+
+function clearPriorWeapon(){
+    playerCard.innerHTML = '';
+    cpuCard.innerHTML = '';
+}
+
+function startGame(playerWeapon){
+    clearPriorWeapon();
+    playerChoice(playerWeapon);
+    cpuChoice();
+
+    if (playerWeapon === cpuWeapon) {
+        playerResults.innerHTML = `<h2>Tied</>`
+        cpuResults.innerHTML = `<h2>Tied</>`
+      } else if (
+        (playerWeapon === "rock" && cpuWeapon === "scissors") || 
+        (playerWeapon === "paper" && cpuWeapon === "rock") || 
+        (playerWeapon === "scissors" && cpuWeapon === "paper")
+      ) {
+        playerResults.innerHTML = `<h2>Winner</>`
+        cpuResults.innerHTML = `<h2>Loser</>`
+      } else {
+        playerResults.innerHTML = `<h2>Loser</>`
+        cpuResults.innerHTML = `<h2>Winner</>`      }
+
+    console.log("player weapon: " + playersWeapon);
+    console.log("cpu weapon:" + cpuWeapon);
+}
 
 
+rock.addEventListener('click', () => startGame("rock"));
+paper.addEventListener('click', () =>  startGame("paper"));
+scissors.addEventListener('click', () =>  startGame("scissors"));
